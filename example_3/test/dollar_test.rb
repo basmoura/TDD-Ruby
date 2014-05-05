e17b5acd745a810d4ea1dd71ab3aa4bc8cbc991c
Breno Moura <basmoura@gmail.com>
2014-05-05 11:21:16 -0300

    Puts back test_multiplication

--- example_3/test/dollar_test.rb
+++ example_3/test/dollar_test.rb
@@ -2,6 +2,15 @@ require './test_helper'
 require './example_3/lib/dollar'
 
 class DollarTest < Minitest::Test
+  def test_multiplication
+    five = Dollar.new(5)
+    product = five.times(2)
+    assert_equal(10, product.amount)
+
+    product = five.times(3)
+    assert_equal(15, product.amount)
+  end
+
   def test_equality
     assert true if Dollar.new(5).equals(Dollar.new(5))
     assert false if Dollar.new(5).equals(Dollar.new(6))
